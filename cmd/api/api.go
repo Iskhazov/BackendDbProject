31169a2a2facfe91b5c7ffbd6a21ebd28df3f8ad
Iskhazov <iskhazovt@mail.ru>
2025-03-13 19:06:41 +0600

    Db Connection & envs

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,1 +1,31 @@
 package api
+
+import (
+	"awesomeProject/service/user"
+	"database/sql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Server struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewServer(addr string, db *sql.DB) *Server {
+	return &Server{
+		addr: addr,
+		db:   db,
+	}
+}
+func (s *Server) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userHandler := user.NewHandler()
+	userHandler.RegisterRoutes(subrouter)
+
+	log.Println("Listening on ", s.addr)
+	return http.ListenAndServe(s.addr, router)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,41 @@
-package cmd
+package main
+
+import (
+	"awesomeProject/cmd/api"
+	"awesomeProject/config"
+	"awesomeProject/db"
+	"database/sql"
+	"github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBUser,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: Successfully connected!")
+}
